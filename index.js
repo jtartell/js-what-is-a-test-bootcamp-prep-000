1576aa18d79868657d3c4ebc811475608f23e15e
Jamie Tartell <jetartell@gmail.com>
2020-01-26 21:23:16 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // write your code below!
 var Name = "Susan"
 var Height = 30
-var message = `${name} is ${height} inches tall`
+var Messge = `${name} is ${height} inches tall`
 
 module.exports = {name, height, message}
 console.log(message)
